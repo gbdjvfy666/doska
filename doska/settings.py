f2f3a9e3f9de32f00e5e17a9f06f6b2b246d3e88
lepeha <nostresko@gmail.com>
2024-06-01 20:45:08 +0400

    css

--- doska/settings.py
+++ doska/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'app',
     "django.contrib.sites",
     "allauth",
@@ -138,18 +139,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/static'),
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
